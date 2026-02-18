01bb37595141fb3827e8c1b73428a50346531d0b
ngoundj-ba <ngoundjb@gmail.com>
2026-02-18 15:50:34 +0000

    principale

---
+++ exoversionning.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+    int n,x,somme=0,cpt=0,i;
+    do{
+    printf("veuillez saisir n entier");
+    scanf("%d",&n);
+    }while(n<0);
+    for(i=0;i<n;i++){
+        do{
+            printf("saisir une valeur negatif");
+            scanf("%d",&x);
+        }while(n>0);
+         if(x%2==0) {
+            somme=+x;
+            cpt++;
+        }
+    }
+    moy=somme/cpt;
+    if(moy%2==0) {
+
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
